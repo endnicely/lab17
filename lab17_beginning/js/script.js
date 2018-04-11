6da05b2ae5322594ea4801a20d8aae1820ba76e5
endnicely <zhoujie66@yahoo.com>
2018-04-10 21:40:26 -0700

    jukebox
    jukebox

--- lab17_beginning/js/script.js
+++ lab17_beginning/js/script.js
@@ -42,25 +42,84 @@
 //                   window.console.log(Car.make);
 ////window.console.log(camero);
 
-var Animal = {
-    walk: function () {},
-    run: function () {},
-    sleep: function () {},
-    jump: function () {},
+//var Animal = {
+//    walk: function () {},
+//    run: function () {},
+//    sleep: function () {},
+//    jump: function () {},
+//    
+//};
+//var Cat = Object.create(Animal, {
+//     meow: {value:"Meow"}
+//        
+//});
+//        
+//           
+// var  Dog = Object.create(Animal, {
+//    bark: function(){
+//        window.alert("Hi");
+//
+//    }
+//        
+//});
+//window.console.log(Cat.meow);
+//window.console.log(Dog.bark);
+
+var Jukebox = function () {
+    "use strict";
+    var albums = [], self;
+    self = {
+        addAlbum: function (album) {
+            albums.push(album);
+            
+        },
+        favoriteAlbum: function () {
+            var max = 0, fav, i;
+            for(i=0;i<albums.length;i +=1){
+                if(albums[i].played > max) {
+                    max = albums[i].played;
+                    fav = albums[i];
+                }
+            }
+            return fav.display();
+        }
+    };
+    return self;
     
 };
-var Cat = Object.create(Animal, {
-     meow: {value:"Meow"}
-        
-});
-        
-           
- var  Dog = Object.create(Animal, {
-    bark: function(){
-        window.alert("Hi");
 
-    }
+var Album = function (artist, album) {
+    "use strict";
+    var self = {
+        played: 0,
+        play: function () {
+            this.played += 1;
+        },
+        display: function () {
+//            return artist + " \"" + album + "hasbeen played " + this.palyed + " times.";
+            return artist + " \"" + album + "hasbeen played " + this.palyed + " times.";
+        }
         
-});
-window.console.log(Cat.meow);
-window.console.log(Dog.bark);
+    };
+    return self;
+    
+};
+var junkebox = new Jukebox();
+var album1 = new Album("Shakira","Knock on my Door");
+var album2 = new Album("Tribe Called Quesst", "The Low End Theory");
+var album3 = new Album("Imagine Dragons", "Hell and Silence");
+
+junkebox.addAlbum(album1);
+junkebox.addAlbum(album2);
+junkebox.addAlbum(album3);
+
+//PLay Albums
+album1.play();
+album1.play();
+album1.play();
+album2.play();
+album3.play();
+album1.play();
+album3.play();
+
+window.console.log("Your favorite album is " + junkebox.favoriteAlbum());
